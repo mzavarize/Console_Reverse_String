2fadddd980402d8834590ae318631456f9d19437
m_zav <m_zav@DESKTOP-MB3SB77>
2023-04-25 22:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Console_Reverse_String;
+
+Console.WriteLine("Aplicação para Inverter String\r\n\r\n ");
+Console.Write("Entre com uma palavra: ");
+string? strInput = Console.ReadLine();
+
+if (String.IsNullOrEmpty(strInput))
+    Console.WriteLine("Erro. Você não forneceu nenhuma palavra!");
+else
+    Console.WriteLine($"A palavra inversa é {new ReverseString().Reverse(strInput)}");
+
+Console.WriteLine();
+Console.WriteLine("Pressione Qualquer tecla para Terminar");
+Console.ReadKey();
---
+++ ReverseString.cs
@@ -0,0 +1,22 @@
+﻿namespace Console_Reverse_String;
+public class ReverseString
+{
+    public string Reverse(string strInput)
+    {
+        return ReversePosition(strInput, 0);
+    }
+
+    private string ReversePosition(string strInput, int Pos)
+    {
+        string strOutput = null!;
+        if (strInput.Length > Pos)
+        {
+            strOutput = ReversePosition(strInput, Pos+1);
+            strOutput += strInput[Pos];
+        }
+        return strOutput;
+
+
+    }
+}
+
